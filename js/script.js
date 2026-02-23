925baa02d6fbd1eea903ffd4e8b656892b9deba2
Rodrigo Noales <rodrigonoales@centraldereservas.com>
2026-02-23 18:20:53 -0300

    Refactor script.js to streamline event handling and improve code readability

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 "use strict";
 
-const itemAnim = document.querySelectorAll('.animatable');
-const imgAll = document.querySelectorAll('img');
-const imgLazy = document.querySelectorAll('.lazy');
 const showFeature = document.querySelector('.showFeature');
-const itemDecoAnim = document.querySelectorAll('.itemDecoAnim');
-const itemCountry = document.querySelector('.world--container');
-const itemCountryContent = document.querySelectorAll('.world--container ul li');
-
-
 
 // More about
 const more = document.querySelector('#more');
@@ -39,31 +31,18 @@ itemNav.forEach(function (item) {
   });
 });
 
-// ...eliminada animación UI blocks IO...
-
-// ...eliminada animación Country UI...
-
-// ...eliminada animación Main deco IO...
-
-// ...eliminada lógica de lazy load de imágenes...
-
-// ...eliminada lógica de sticky menu JS...
-
-
 // Feature
 let cover = '';
 let coverActive = '';
 let root = document.documentElement;
-let initX = root.style.setProperty('--xpos', 150);
-let initY = root.style.setProperty('--ypos', 350);
-let imgSrc = '';
+root.style.setProperty('--xpos', 150);
+root.style.setProperty('--ypos', 350);
 
 function getFeature(itemNavId) {
   const closeLink = document.querySelector('.close');
   if (itemNavId === 'layout') {
     fetch('./content/layout.html')
     .then(function (response) {
-      console.log(response.ok);
       return response.text();
     })
     .then(function (data) {
@@ -73,7 +52,6 @@ function getFeature(itemNavId) {
   } else if (itemNavId === 'rwd') {
     fetch('./content/rwd.html')
     .then(function (response) {
-      console.log(response.ok);
       return response.text();
     })
     .then(function (data) {
@@ -83,7 +61,6 @@ function getFeature(itemNavId) {
   } else if (itemNavId === 'css') {
     fetch('./content/css.html')
     .then(function (response) {
-      console.log(response.ok);
       return response.text();
     })
     .then(function (data) {
@@ -93,7 +70,6 @@ function getFeature(itemNavId) {
   } else if (itemNavId === 'music') {
     fetch('./content/music.html')
     .then(function (response) {
-      console.log(response.ok);
       return response.text();
     })
     .then(function (data) {
@@ -103,7 +79,6 @@ function getFeature(itemNavId) {
   } else if (itemNavId === 'ui') {
     fetch('./content/ui.html')
     .then(function (response) {
-      console.log(response.ok);
       return response.text();
     })
     .then(function (data) {
@@ -131,7 +106,6 @@ let featureUI = function (itemNavId, closeLink) {
     if (coverActive === false && itemNavId !== 'music' && itemNavId !== 'css') {
       clearTimeout(time);
       move(itemNavId);
-      console.log('cleared');
     }
   }, 1000);
 }
@@ -140,7 +114,7 @@ let featureUI = function (itemNavId, closeLink) {
 function move(itemNavId) {
   let wait = false;
   let placeholder = document.querySelector('.contentItem-' + itemNavId + ' .contentItem--wrapper .placeholder');
-  let listener = placeholder.addEventListener(('mouseenter', 'mousemove'), e => {
+  placeholder.addEventListener('mousemove', e => {
     if (!wait) {
       wait = true;
       root.style.setProperty('--xpos', -e.clientX + (screen.width - placeholder.offsetWidth)  + "px");
@@ -148,19 +122,17 @@ function move(itemNavId) {
       setTimeout(function () { wait = false; }, 25);
     }
   })
-  placeholder.removeEventListener(('mouseout', 'mouseleave'), listener);
 }
 
 // Close feature modal
 function close(e) {
   e.preventDefault();
   showFeature.classList = 'showFeature';
-  e.itemNavId = '';
   if (cover !== null) {
     cover.classList.remove('is-active');
   }
-  initX = root.style.setProperty('--xpos', 150);
-  initY = root.style.setProperty('--ypos', 350);
+  root.style.setProperty('--xpos', 150);
+  root.style.setProperty('--ypos', 350);
   window.scrollBy(0, -window.innerHeight/2);
 }
 
@@ -181,10 +153,3 @@ function subRun() {
     }, true)
   })
 }
-
-// ...eliminada animación swirl decoration GSAP...
-
-
-// ...eliminada animación de intro GSAP...
-
-
